9e6168a014f1043920d8d888dd273fe22df94bee
mituko <mituko.misawa@yahoo.co.jp>
2021-07-15 11:27:15 +0000

    microposts favorites 修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,12 +9,12 @@
         <div class="col-sm-8">
             {{-- タブ --}}
             @include('users.navtabs')
-            @if (Auth::id() == $user->id)
+            
                 {{-- 投稿フォーム --}}
                 @include('microposts.form')
                 {{-- 投稿一覧 --}}
                 @include('microposts.microposts')
-            @endif
+           
                 
         </div>
     </div>
